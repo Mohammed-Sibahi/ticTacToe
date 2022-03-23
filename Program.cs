06bdd8244d05bacb7c470aa30fdc1405b354120f
MAS <sibahi@live.com>
2022-03-23 16:36:04 +0400

    greeting and providing instructions

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. provide instructions
+            // 1.1 A greeting
+            Console.WriteLine("Welcome to Tic Tac Toe Game!");
+
+            // 1.2 Display player sign, player 1 is X and player 2 is O
+            Console.WriteLine("Player 1: X");
+            Console.WriteLine("Player 2: O");
+            Console.WriteLine();
+
+        }
+    }
+}
